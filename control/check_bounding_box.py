8a0889db2d2ff49940059f4c122084617d8068b8
luzhangao <luzhangao@gmail.com>
2021-11-07 22:12:18 -0500

    Add a new function to display train and val images

--- control/check_bounding_box.py
+++ control/check_bounding_box.py
@@ -10,6 +10,7 @@ Display the picture with bounding box
 
 
 import cv2
+import pandas as pd
 from matplotlib import pyplot as plt
 
 
@@ -39,6 +40,18 @@ def visualize_bbox(img, bbox, class_name, color=BOX_COLOR, thickness=2):
 
 
 def visualize(image, bboxes, category_ids, category_id_to_name):
+    """
+
+    :param image:
+    :param bboxes: list
+           e.g. [[586.23, 324.18, 16.15, 38.93]]
+    :param category_ids: list
+           e.g. [44]
+    :param category_id_to_name: dict
+           e.g. {1: 'person', 2: 'bicycle', 3: 'car', ...}
+    :return:
+    """
+    print(bboxes, category_ids, category_id_to_name)
     img = image.copy()
     for bbox, category_id in zip(bboxes, category_ids):
         class_name = category_id_to_name[category_id]
@@ -50,4 +63,51 @@ def visualize(image, bboxes, category_ids, category_id_to_name):
 
 
 if __name__ == '__main__':
-    pass
+    import os
+    from control.parse_annotations import yolo_to_coco
+    image_sample_paths = {
+        "val2017": "../raw_data/coco2017/images/sample_val2017/",
+        "train2017": "../raw_data/coco2017/images/sample_train2017/",
+        # "test2017": "../raw_data/coco2017/images/sample_test2017/"
+    }
+    label_paths = {
+        "val2017": "../raw_data/coco2017/labels/val2017/",
+        "train2017": "../raw_data/coco2017/labels/train2017/",
+        # "test2017": "../raw_data/coco2017/labels/test2017/",
+        "coco128": "../datasets/coco128/labels/train2017/"
+    }
+    label_sample_paths = {
+        "val2017": "../raw_data/coco2017/labels/sample_val2017/",
+        "train2017": "../raw_data/coco2017/labels/sample_train2017/",
+        # "test2017": "../raw_data/coco2017/labels/sample_test2017/",
+    }
+
+    key = "train2017"
+    # key = "val2017"
+    image_path = image_sample_paths[key]
+    label_path = label_sample_paths[key]
+    # names: ["person", "car", "chair", "book", "bottle"] # class names
+
+    category_id_to_name = {0: 'person', 1: 'car', 2: 'chair', 3: 'book', 4: 'bottle'}
+
+    for filename in os.listdir(label_path):
+        """
+        The content of the text file:
+        2 0.670773 0.262381 0.055859 0.101052
+        2 0.94168 0.212268 0.034734 0.090887
+        0 0.673516 0.596629 0.553469 0.775278
+        0 0.476828 0.292237 0.352094 0.574784
+        """
+        df = pd.read_csv(label_path + filename, header=None, sep=" ")
+        df.columns = ["category_id", "x_center", "y_center", "width", "height"]
+        # print(df)
+        category_ids = df["category_id"].tolist()
+        image_file_name = filename[: len(filename)-3] + "jpg"
+        current_image = cv2.imread(image_path + image_file_name)
+        df["bbox"] = df.apply(lambda x: yolo_to_coco([x["x_center"], x["y_center"], x["width"], x["height"]], current_image.shape[1], current_image.shape[0]), axis=1)
+        bboxes = df["bbox"].tolist()
+        visualize(current_image, bboxes, category_ids, category_id_to_name)
+
+
+
+
